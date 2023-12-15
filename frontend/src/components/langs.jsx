d8755ec432333752efe13a2190f9a4a30f53167a
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-15 11:19:51 +0200

    added all languages

--- frontend/src/components/langs.jsx
+++ frontend/src/components/langs.jsx
@@ -1,5 +1,7 @@
 import "./styles/langs.css"
 
+import NginxLogo from "../assets/images/langs/nginx-icon.svg"
+import TraefikLogo from "../assets/images/langs/traefikio-icon.svg"
 
 export default function Langs() {
     const languages_info = [
@@ -7,7 +9,66 @@ export default function Langs() {
             name: "React",
             img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
         },
-
+        {
+            name: "HTML",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+            name: "CSS",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        },
+        {
+            name: "SASS",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+        },
+        {
+            name: "Tailwind",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        },
+        {
+            name: "Bootstrap",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+        },
+        {
+            name: (<>Java<br />Script</>),
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        },
+        {
+            name: "Python",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        },
+        {
+            name: "Django",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+        },
+        {
+            name: "Postgres",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+        },
+        {
+            name: "Docker",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+        },
+        {
+            name: "Nginx",
+            img: NginxLogo,
+        },
+        {
+            name: "traefik",
+            img: TraefikLogo,
+        },
+        {
+            name: "Git",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        },
+        {
+            name: "GitHub",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+        {
+            name: "GitHub",
+            img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-plain.svg",
+        },
     ]
     return (
         <div className="bg-pink w-full">
