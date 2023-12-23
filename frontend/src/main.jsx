a655b9fb7a6cad637e177807fee0c2e3d37c8aee
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-23 13:29:06 +0200

    added react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+    createRoutesFromElements,
+    createBrowserRouter,
+    Route,
+    RouterProvider
+  } from "react-router-dom";
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+          <Route path='/' index element={<App />} />
+    )
+  );
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
+    <RouterProvider router={router} />
 )
