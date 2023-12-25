7513275b521b1cacad8f29e6981032d11bc17345
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-25 12:17:23 +0200

    formating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import './App.css'
+import "./App.css";
 
-import Intro from "./components/intro"
-import Projects from "./components/projects"
-import Langs from "./components/langs"
-import Contact from "./components/contact"
-import RubiksCube from "./components/rubikscube"
+import Intro from "./components/intro";
+import Projects from "./components/projects";
+import Langs from "./components/langs";
+import Contact from "./components/contact";
+import RubiksCube from "./components/rubikscube";
 
 function App() {
-
-  return (
-    <>
-      <Intro />
-      <Projects />
-      <Langs />
-      <Contact />
-      {/* <RubiksCube /> */}
-    </>
-  )
+	return (
+		<>
+			<Intro />
+			<Projects />
+			<Langs />
+			<Contact />
+			{/* <RubiksCube /> */}
+		</>
+	);
 }
 
-export default App
+export default App;
