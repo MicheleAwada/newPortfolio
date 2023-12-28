22159371e9f3b1057fa16cdbed5c1faf2dbd5440
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-28 15:32:42 +0200

    projects page items now clickable

--- frontend/src/components/projects.jsx
+++ frontend/src/components/projects.jsx
@@ -7,6 +7,8 @@ import mugThumbnail from "../assets/projects/images/mug.png";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+import { IoMdArrowBack } from "react-icons/io";
+
 export default function Project() {
 	const states = [
 		[
@@ -52,12 +54,6 @@ export default function Project() {
 			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: 0},
 		],
 	];
-	const constantStateStyles = [
-		{zIndex:1},
-		{zIndex:3},
-		{zIndex:2},
-		{zIndex:0},
-	]
 
 	states.forEach((state, mainIndex) => {
 		state.forEach((componentState, componentStateIndex) => {
@@ -87,26 +83,60 @@ export default function Project() {
 		})
 	})
 	const [projectsType, setProjectsType] = useState(0)
+	const [selectedProject, setSelectedProject] = useState(null)
+	const [prevSelectedProject, setPrevSelectedProject] = useState(null)
 	function choiceIndex(array) {
 		return Math.floor(Math.random() * array.length)
 	}
 	useEffect(() => {
 		const projectsInterval = setInterval(() => {
-			const newProjectTypeIndex = choiceIndex(states) 
-			setProjectsType(newProjectTypeIndex)
-		}, 3000)
+			if (selectedProject===null) {const newProjectTypeIndex = choiceIndex(states) 
+			setProjectsType(newProjectTypeIndex)}
+		}, 3500)
 		return () => {
 			clearInterval(projectsInterval)
 		}
 	})
+
+	const selectedProjectStyles = {top:0, left:0, width: "100%", height:"100%", zIndex:10}
+
+	const constantStateStyles = [
+		{zIndex:1},
+		{zIndex:1},
+		{zIndex:1},
+		{zIndex:0},
+	]
+
+	const projects_mapping = [
+		{className: "mini-projects mug-project bg-[#A9D5E5]"},
+		{className: "mini-projects kentucky-project bg-pink"},
+		{className: "mini-projects chess-project bg-purple"},
+		{className: "mini-projects friendlyfur-project bg-dark-purple"},
+	]
+
 	return (
 		<div className="bg-dark-white w-full flex items-center justify-center">
-			<div className="projects-container my-20 w-[100%] sm:w-[95%] lg:w-[80%] xl:w-[70%] 2xl:[w-55%] flex items-center justify-center">
+			<div className="projects-container my-20 w-[97%] sm:w-[93%] lg:w-[81%] xl:w-[73%] 2xl:[w-55%] flex items-center justify-center">
 				<div className="square-projects-container">
-					<div style={{...constantStateStyles[0], ...states[projectsType][0]}} className="mini-projects mug-project bg-cyan-500"></div>
-					<div style={{...constantStateStyles[1], ...states[projectsType][1]}} className="mini-projects kentucky-project bg-pink"></div>
-					<div style={{...constantStateStyles[2], ...states[projectsType][2]}} className="mini-projects chess-project bg-purple"></div>
-					<div style={{...constantStateStyles[3], ...states[projectsType][3]}} className="mini-projects friendlyfur-project bg-dark-purple"></div>
+					{projects_mapping.map((props, index) => {
+						const isSelected = selectedProject===index
+						const isPrevSelected = prevSelectedProject===index
+						return <div key={index} style={{...constantStateStyles[index], ...(isPrevSelected ? {zIndex: 5} : {}), ...(isSelected ? selectedProjectStyles : states[projectsType][index])}} {...props} onClick={() => {
+							if (selectedProject===null) {
+								setSelectedProject(index)
+								setPrevSelectedProject(index)
+							}
+						}}>
+							{isSelected && <IoMdArrowBack color="white" size={"3rem"} className="m-2 cursor-pointer" onClick={() => {
+								if (selectedProject!==null) {
+									setSelectedProject(null)
+									setTimeout(() => {
+										setPrevSelectedProject(null)
+									}, 1000)
+								}
+								}} />}
+						</div>
+					})}
 				</div>
 			</div>
 		</div>
