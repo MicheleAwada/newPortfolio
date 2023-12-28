ba540adc3dc1f1d5514aaa9e783f9cc99c81f1dc
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-28 13:52:44 +0200

    new amazing project page

--- frontend/src/components/projects.jsx
+++ frontend/src/components/projects.jsx
@@ -5,108 +5,110 @@ import kentuckyThumbnail from "../assets/projects/images/kentucky.png";
 import chessThumbnail from "../assets/projects/images/chess.png";
 import mugThumbnail from "../assets/projects/images/mug.png";
 import { Link } from "react-router-dom";
-
-export function OldProjects() {
-	const projects_info = [
-		{
-			title: "Mug",
-			description: (
-				<p className="text-xs">
-					Lightning-fast social media app on React with Django backend. Login,
-					post, edit, delete, like, comment, follow, view, and more! Change
-					password & auth info seamlessly.
-				</p>
-			),
-			thumbnail: mugThumbnail,
-			link: (
-				<a
-					href="http://mug.micheleawada.com"
-					className="mx-auto bg-red-600 text-white px-4 py-2 rounded-xl"
-				>
-					LIVE Site
-				</a>
-			),
-		},
-		{
-			title: "Friendly Fur",
-			description: <p>Website Developer</p>,
-			thumbnail: friendlyFurThumbnail,
-			link: (
-				<Link
-					to="kentucky/"
-					className="bg-red-600 text-white px-4 py-2 rounded-xl"
-				>
-					Play NOW!
-				</Link>
-			),
-		},
-		{
-			title: "Kentucky Game",
-			description: <p>Website Developer</p>,
-			thumbnail: kentuckyThumbnail,
-			link: (
-				<Link
-					to="kentucky/"
-					className="bg-red-600 text-white px-4 py-2 rounded-xl"
-				>
-					Play NOW!
-				</Link>
-			),
-		},
-		{
-			title: "Chess Game",
-			description: <p>Website Developer</p>,
-			thumbnail: chessThumbnail,
-			link: (
-				<Link
-					to="kentucky/"
-					className="bg-red-600 text-white px-4 py-2 rounded-xl"
-				>
-					Play NOW!
-				</Link>
-			),
-		},
-	];
-	return (
-		<section className="bg-dark-white w-full pt-4">
-			<h1 className="text-center text-dark-purple text-4xl">
-				<span className="rotated-my">My</span> Projects
-			</h1>
-			<div
-				id="proj-container"
-				className="flex flex-wrap gap-8 p-8 items-center w-full py-12"
-			>
-				{projects_info.map((info, index) => (
-					<div
-						key={index}
-						className="flip-card w-[22rem] h-[16rem] p-4 bg-transparent border-dark-purple border-2 rounded-md"
-					>
-						<div className="flip-card-inner rounded-md">
-							<div className="flip-card-front flex flex-col h-full rounded-md overflow-hidden">
-								<img
-									src={info.thumbnail}
-									alt={info.title + " Thumbnail"}
-									className="w-full aspect-video object-contain bg-dark-white"
-								/>
-								<div className="w-full flex-grow bg-purple flex items-center justify-center">
-									<p className="text-white text-2xl">{info.title}</p>
-								</div>
-							</div>
-							<div className="flip-card-back flex justify-center items-center rounded-md ">
-								<div className="flex flex-col gap-2">
-									<h1 className="text-xl">{info.title}</h1>
-									{info.description}
-									{info.link}
-								</div>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</section>
-	);
-}
+import { useEffect, useState } from "react";
 
 export default function Project() {
-	return <div className="projects-container"></div>;
+	const states = [
+		[
+			{ width:25, height:100, top: 0, left: 0, bottom: null, right: null, borderRadius: 0 },
+			{ width:25, height:100, top: 0, left: 25, bottom: null, right: null, borderRadius: 0 },
+			{ width:25, height:100, top: 0, left: null, bottom: null, right: 25, borderRadius: 0 },
+			{ width:25, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: 0 },
+		],
+		[
+			{ width:25, height:100, top: 0, left: 0, bottom: null, right: null, borderRadius: "100vmax" },
+			{ width:25, height:100, top: 0, left: 25, bottom: null, right: null, borderRadius: "100vmax" },
+			{ width:25, height:100, top: 0, left: null, bottom: null, right: 25, borderRadius: "100vmax" },
+			{ width:25, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: "100vmax" },
+		],
+		[
+			{ width:25, height:66.66, top: 0, left: 0, bottom: null, right: null, borderRadius: 0 },
+			{ width:50, height:33.33, top: null, left: 0, bottom: 0, right: null, borderRadius: 0 },
+			{ width:25, height:66.66, top: 0, left: 25, bottom: null, right: null, borderRadius: 0 },
+			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: 0 },
+		],
+		[
+			{ width:25, height:66.66, top: 0, left: 0, bottom: null, right: null, borderRadius: "100vmax 100vmax 0 100vmax" },
+			{ width:50, height:33.33, top: null, left: 0, bottom: 0, right: null, borderRadius: "0 0 100vmax 0" },
+			{ width:25, height:66.66, top: 0, left: 25, bottom: null, right: null, borderRadius: "100vmax 0 0 0" },
+			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: "0 0 0 100vmax" },
+		],
+		[
+			{ width:50, height:50, top: 0, left: 0, bottom: null, right: null, borderRadius: "0 100vmax 100vmax 0" },
+			{ width:25, height:40.45, top: null, left: null, bottom: 0, right: 0, borderRadius: "100vmax" },
+			{ width:50, height:50, top: null, left: 0, bottom: 0, right: null, borderRadius: "4rem 0.75rem 4rem 0.75rem" },
+			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: "100vmax 0 0 0" },
+		],
+		[
+			{ width:25, height:100, top: 0, left: 0, bottom: null, right: null, borderRadius: "1rem"},
+			{ width:25, height:40.45, top: null, left: null, bottom: 0, right: 0, borderRadius: "1rem"},
+			{ width:25, height:100, top: 0, left: 25, bottom: null, right: null, borderRadius: "1rem"},
+			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: "1rem"},
+		],
+		[
+			{ width:25, height:100, top: 0, left: 0, bottom: null, right: null, borderRadius: 0	},
+			{ width:25, height:40.45, top: null, left: null, bottom: 0, right: 0, borderRadius: 0},
+			{ width:25, height:100, top: 0, left: 25, bottom: null, right: null, borderRadius: 0},
+			{ width:50, height:100, top: 0, left: null, bottom: null, right: 0, borderRadius: 0},
+		],
+	];
+	const constantStateStyles = [
+		{zIndex:1},
+		{zIndex:3},
+		{zIndex:2},
+		{zIndex:0},
+	]
+
+	states.forEach((state, mainIndex) => {
+		state.forEach((componentState, componentStateIndex) => {
+			if (componentState["right"]!==null && componentState["left"]===null) {
+				const oldRight = componentState["right"]
+				states[mainIndex][componentStateIndex]["right"] = null
+				states[mainIndex][componentStateIndex]["left"] = 100-oldRight-componentState["width"]
+			}
+			if (componentState["bottom"]!==null && componentState["top"]===null) {
+				const oldBottom = componentState["bottom"]
+				states[mainIndex][componentStateIndex]["bottom"] = null
+				states[mainIndex][componentStateIndex]["top"] = 100-oldBottom-componentState["height"]
+			}
+			componentState = states[mainIndex][componentStateIndex]
+			Object.keys(componentState).forEach((key) => {
+				// const current = states[mainIndex][componentStateIndex][key]
+				if (states[mainIndex][componentStateIndex][key]===null) {
+					delete states[mainIndex][componentStateIndex][key]
+				}
+				if (key==="width" || key==="height" || key==="top" || key==="bottom" || key==="left" || key==="right" || key==="borderRadius") {
+					if (states[mainIndex][componentStateIndex][key]!==null && typeof states[mainIndex][componentStateIndex][key]==="number") {
+						states[mainIndex][componentStateIndex][key] = states[mainIndex][componentStateIndex][key] + "%"
+					}
+				}
+			})
+
+		})
+	})
+	const [projectsType, setProjectsType] = useState(0)
+	function choiceIndex(array) {
+		return Math.floor(Math.random() * array.length)
+	}
+	useEffect(() => {
+		const projectsInterval = setInterval(() => {
+			const newProjectTypeIndex = choiceIndex(states) 
+			setProjectsType(newProjectTypeIndex)
+		}, 3000)
+		return () => {
+			clearInterval(projectsInterval)
+		}
+	})
+	return (
+		<div className="bg-dark-white w-full flex items-center justify-center">
+			<div className="projects-container my-20 w-[100%] sm:w-[95%] lg:w-[80%] xl:w-[70%] 2xl:[w-55%] flex items-center justify-center">
+				<div className="square-projects-container">
+					<div style={{...constantStateStyles[0], ...states[projectsType][0]}} className="mini-projects mug-project bg-cyan-500"></div>
+					<div style={{...constantStateStyles[1], ...states[projectsType][1]}} className="mini-projects kentucky-project bg-pink"></div>
+					<div style={{...constantStateStyles[2], ...states[projectsType][2]}} className="mini-projects chess-project bg-purple"></div>
+					<div style={{...constantStateStyles[3], ...states[projectsType][3]}} className="mini-projects friendlyfur-project bg-dark-purple"></div>
+				</div>
+			</div>
+		</div>
+	);
 }
