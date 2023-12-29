f0e405ea548add8297b77f9b4d2465116ca4b2a8
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-29 17:17:08 +0200

    added resume to assets instead of public

--- frontend/src/components/intro.jsx
+++ frontend/src/components/intro.jsx
@@ -6,6 +6,8 @@ import wave from "../assets/intro/images/wave.svg";
 
 import KUTE from "kute.js";
 
+import ResumeURL from "../assets/intro/Michele Awada's Resume.pdf?url";
+
 function ButtonBubble(props) {
 	const { className, ...rest } = props;
 
@@ -128,7 +130,7 @@ export default function Intro() {
 						<a
 							className="px-4 py-2  text-dark-purple bg-dark-white rounded-full text-2xl font-medium"
 							target="_blank"
-							href={`/Michele_Awada's_Resume.pdf`}
+							href={ResumeURL}
 						>
 							Resume
 						</a>
