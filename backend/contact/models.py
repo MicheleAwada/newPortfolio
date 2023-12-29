e8f48cf29dfa1c65a6cf192bea23535126e3b304
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-29 10:17:31 +0200

    changed required fields

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=256)
-    email = models.EmailField(max_length=256,blank=True)
-    title = models.CharField(max_length=200,blank=True)
+    email = models.EmailField(max_length=256)
+    title = models.CharField(max_length=200)
     body = models.TextField(blank=True)
 
     created_on = models.DateTimeField(auto_now_add=True)
