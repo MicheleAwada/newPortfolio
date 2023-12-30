47380050d6d8074640713e5b3e65c117a32c8332
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-30 20:05:29 +0200

    fixed sending email

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -155,7 +155,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_EMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
