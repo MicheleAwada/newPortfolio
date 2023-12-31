af8c939e610a21f57268b163a6ee00c0dcbd432d
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-31 09:52:42 +0200

    added send email on contact

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -19,7 +19,7 @@ class ContactViewSet(mixins.CreateModelMixin, GenericViewSet):
             f"The message was from (Name): {request.data.get("name", "unknown")}, and the email {request.data.get('email', 'unknown')}\nThe Title is: {request.data.get('title', 'unknown')}\n\nAnd the body is below\n\n{request.data.get('body', 'unknown')}",
             settings.EMAIL_HOST_USER,
             [settings.EMAIL_TO_SEND],
-        )
+            )
         except:
             pass
         return super().create(request, *args, **kwargs)
