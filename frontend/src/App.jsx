f63d595b8bc75c034d9e31d2271bdc8fb293ab07
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-03 08:24:15 +0200

    use state with theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,17 +9,20 @@ import { useEffect, useState } from "react";
 function App() {
 	const [messages, setMessages] = useState({});
 	const simpleAddMessage = getSimpleAddMessages(messages, setMessages);
-	useEffect(() => {
-		function setThemePreference() {
-			const prefersDarkMode =
-				window.matchMedia &&
-				window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-			const root = document.documentElement;
-			root.setAttribute("data-theme", prefersDarkMode ? "dark" : "light");
-		}
-		setThemePreference();
-	});
+	const [isLightTheme, setIsLightTheme] = useState(true);
+
+	useEffect(() => {
+		const prefersDarkMode =
+			window.matchMedia &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches;
+		setIsLightTheme(!prefersDarkMode);
+	}, []);
+
+	useEffect(() => {
+		const root = document.documentElement;
+		root.setAttribute("data-theme", isLightTheme ? "light" : "dark");
+	}, [isLightTheme]);
 	return (
 		<>
 			<RenderMessages messages={messages} />
