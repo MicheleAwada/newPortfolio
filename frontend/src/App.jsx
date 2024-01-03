897dbda42fba4b51b226ab7d66f3a523e47e82ea
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-03 08:11:05 +0200

    added data theme to identify what theme to use

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,22 @@ import Langs from "./components/langs";
 import Contact from "./components/contact";
 
 import RenderMessages, { getSimpleAddMessages } from "./components/messages";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 	const [messages, setMessages] = useState({});
 	const simpleAddMessage = getSimpleAddMessages(messages, setMessages);
+	useEffect(() => {
+		function setThemePreference() {
+			const prefersDarkMode =
+				window.matchMedia &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+			const root = document.documentElement;
+			root.setAttribute("data-theme", prefersDarkMode ? "dark" : "light");
+		}
+		setThemePreference();
+	});
 	return (
 		<>
 			<RenderMessages messages={messages} />
