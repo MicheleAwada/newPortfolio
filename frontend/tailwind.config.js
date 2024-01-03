045ed08f8f1450b3d6c083a8f08aa1d4b6e397e6
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-03 07:56:32 +0200

    layed waves colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+	content: [
 		"./index.html",
 		"./src/**/*.{js,jsx,ts,tsx}",
 		"./src/**/*.svg",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+		"node_modules/flowbite-react/lib/esm/**/*.js",
 	],
-  theme: {
-    extend: {
-      colors: {
-        "dark-white": "var(--color-dark-white)",
-        "pink": "var(--color-pink)",
-        "purple": "var(--color-purple)",
-        "dark-purple": "var(--color-dark-purple)",
-      }
-    },
-  },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
-}
-
+	theme: {
+		extend: {
+			colors: {
+				"dark-white": "var(--color-dark-white)",
+				"dark-white-to-pink-1": "var(--color-dark-white-to-pink-1)",
+				"dark-white-to-pink-2": "var(--color-dark-white-to-pink-2)",
+				"dark-white-to-pink-3": "var(--color-dark-white-to-pink-3)",
+				pink: "var(--color-pink)",
+				purple: "var(--color-purple)",
+				"dark-purple": "var(--color-dark-purple)",
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
